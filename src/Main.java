28a4f735223ec90f59cb98631b501cdb0c70eac5
Victor Santelé <Vic16@hotmail.be>
2021-04-21 22:17:16 +0200

    test dbAccess

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,18 @@
-import data.DatabaseConnection;
+import data.access.*;
+import model.Charity;
 
-import java.sql.*;
-
+import java.sql.SQLException;
 
 public class Main {
   public static void main(String[] args) {
-      Connection connection = DatabaseConnection.getInstance();
+      BookingDataAccess dao = new BookingDBAccess();
+    try {
+      Charity charity = new Charity("avmp", "À Vos Marques Prêts", "contact@avmp.be","5, Rue des platanes", "Mouscron", "7700", "Belgique");
+      int nb = dao.addCharity(charity);
+      System.out.println(nb);
+    } catch (SQLException throwables) {
+      throwables.printStackTrace();
+    }
+
   }
 }
