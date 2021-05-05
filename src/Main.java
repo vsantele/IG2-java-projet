31d31e2dd189ed0acdafc30a88336995fc33107b
Aude Lekeux <aude.lekeux@gmail.com>
2021-05-05 17:16:36 +0200

    Main stage

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,29 @@
 import data.access.*;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 import model.*;
+import view.*;
 
 import java.util.ArrayList;
 
-public class Main {
-  public static void main(String[] args) {
-      BookingDataAccess dao = new BookingDBAccess();
-      Session ses = new Session(5);
-      ArrayList<Date> arr = dao.getDates(ses);
-      for(Date date : arr) {
-        System.out.println(date);
-      }
-  }
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage){
+        primaryStage.setTitle("Application");
+        primaryStage.setX(200);
+        primaryStage.setY(200);
+        primaryStage.setWidth(800);
+        primaryStage.setHeight(500);
+        primaryStage.show();
+
+        VBox vBox = new VBox(new Label("Booking Table"));
+        Scene scene = new Scene(vBox);
+        primaryStage.setScene(scene);
+    }
 }
--- src/view/MainWindow.java
+++ src/view/MainWindow.java
@@ -1,8 +1,25 @@
 package view;
 
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-public class MainWindow {
+public class MainWindow extends Application {
 
-    Stage window = new Stage();
+    private Parent createContent(){
+        return new StackPane(new Text("Hello World"));
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(createContent(), 300, 300));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
