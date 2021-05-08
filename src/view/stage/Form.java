8187cdc1fa55e89356a24540e8187680b09b7c4e
Victor Santelé <Vic16@hotmail.be>
2021-05-08 13:31:06 +0200

    champs créés

--- src/view/stage/Form.java
+++ src/view/stage/Form.java
@@ -1,22 +1,83 @@
 package view.stage;
 
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.util.converter.NumberStringConverter;
 
 public class Form {
   private Stage stage;
   private Scene scene;
   private GridPane grid;
+
+  private TextField firstnameTextField;
+  private TextField lastnameTextField;
+  private TextField phoneTextField;
+  private TextField emailTextField;
+  private DatePicker birthdatePicker;
+  private TextField amountTextField;
+  private RadioButton isPaidYes;
+  private RadioButton isPaidNo;
+  private ToggleGroup isPaidGroup;
+  private ChoiceBox<String> charityPicker;
+  private ChoiceBox<String> activityPicker;
+  private ChoiceBox<String> sessionPicker;
+  private ChoiceBox<String> datePicker;
+
   
   public Form(Stage primaryStage) {
     stage = new Stage();
     grid = new GridPane();
     scene = new Scene(grid);
-    
+
+    firstnameTextField = new TextField();
+    lastnameTextField = new TextField();
+    phoneTextField = new TextField();
+    emailTextField = new TextField();
+    birthdatePicker = new DatePicker();
+    amountTextField = new TextField();
+    amountTextField.setTextFormatter(new TextFormatter<>(new NumberStringConverter()));
+    isPaidYes = new RadioButton("Oui");
+    isPaidNo = new RadioButton("Non");
+    isPaidGroup = new ToggleGroup();
+    isPaidYes.setToggleGroup(isPaidGroup);
+    isPaidNo.setToggleGroup(isPaidGroup);
+    charityPicker = new ChoiceBox<>();
+    activityPicker = new ChoiceBox<>();
+    sessionPicker = new ChoiceBox<>();
+    datePicker = new ChoiceBox<>();
+
+    datePicker.getItems().add("Date 1");
+    datePicker.getItems().add("Date 2");
+    datePicker.getItems().add("Date 3");
+
+    grid.add(new Label("Prénom: "), 0,0);
+    grid.add(firstnameTextField, 1,0);
+    grid.add(new Label("Nom: "), 0,1);
+    grid.add(lastnameTextField, 1,1);
+    grid.add(new Label("Téléphone: "), 0,2);
+    grid.add(phoneTextField, 1,2);
+    grid.add(new Label("Email: : "), 0,3);
+    grid.add(emailTextField, 1,3);
+    grid.add(new Label("Date de Naissance: "), 0,4);
+    grid.add(birthdatePicker, 1, 4);
+    grid.add(new Label("Montant: "), 0,5);
+    grid.add(amountTextField,1,5);
+    grid.add(new Label("Déjà payé: "), 0,6);
+    grid.add(new HBox(isPaidYes, isPaidNo), 1,6);
+    grid.add(new Label("Association: "), 0,7);
+    grid.add(charityPicker,1,7);
+    grid.add(new Label("Activité: "), 0,8);
+    grid.add(activityPicker, 1,8);
+    grid.add(new Label("Session: "), 0,9);
+    grid.add(sessionPicker, 1,9);
+    grid.add(new Label("Date: "),0,10);
+    grid.add(datePicker, 1,10);
+
+
     stage.initModality(Modality.APPLICATION_MODAL);
     stage.initOwner(primaryStage);
     stage.setTitle("Formulaire");
