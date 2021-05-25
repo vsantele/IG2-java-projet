94a68e7b08201961aeb532afb71cc2fa7de7ea58
Victor Santelé <vic16@hotmail.be>
2021-05-25 19:30:00 +0200

    add Default database information

---
+++ src/util/DatabaseInformation.java
@@ -0,0 +1,24 @@
+package util;
+
+public class DatabaseInformation {
+  private static final String driver = "mysql";
+  //  private static String host = "2001:41d0:404:200::5960";
+  private static final String host = "localhost";
+  private static final int port = 3306;
+  private static final String user = "root";
+  private static final String password = "root";
+  private static final String dbName = "booking";
+  private static final Boolean useSSl = false;
+  
+  public static String getUrl() {
+    return "jdbc:" + driver + "://address=(protocol=tcp)(host=" + host + ")(port=" + port +")/" + dbName + "?useSSl=" + useSSl;
+  }
+  
+  public static String getUser() {
+    return user;
+  }
+  
+  public static String getPassword() {
+    return password;
+  }
+}
